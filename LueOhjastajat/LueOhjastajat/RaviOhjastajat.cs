e7bcfec8b9872edbb3c0cbd9cc7503e5530a325c
EsaSalmik <esa.salmikangas@jamk.fi>
2017-01-12 09:07:23 +0200

    New method LueTiedostoVerSimple added and rename of methods

--- LueOhjastajat/LueOhjastajat/RaviOhjastajat.cs
+++ LueOhjastajat/LueOhjastajat/RaviOhjastajat.cs
@@ -1,4 +1,5 @@
-﻿//sovellus lukee tekstitiedostosta ohjastajatietoja ja näyttää ne konsolilla
+﻿//Esa Salmikangas 11.-12.1.2017
+//sovellus lukee tekstitiedostosta ohjastajatietoja ja näyttää ne konsolilla
 //tekstitiedosto haettu heppa.hippos.fi 11.1.2017
 using System;
 using System.Data;
@@ -24,24 +25,57 @@ namespace JAMK.IT
     {
       try
       {
-        LueTiedostoV0("C:/temp/OhjastajienRavitilasto2016.txt");
+        LueTiedostoVerSimple("C:/temp/OhjastajienRavitilasto2016.txt");
+        //LueTiedostoVerStruct("C:/temp/OhjastajienRavitilasto2016.txt");
       }
       catch (Exception ex)
       {
         Console.WriteLine("Ohjelman suorituksessa tapahtui virhe ", ex.Message);
       }
     }
-    static void LueTiedostoV0(string tiedosto)
+    static void LueTiedostoVerSimple(string tiedosto)
+    {
+      //luetetaan tiedosto läpi rivi kerrallaan
+      try
+      {
+        //luetaan tiedosto ja tallennetaan strukteihin
+        //each line of the text file into a string array. 
+        int counter = 0;
+        string line;
+        // Read the file and display it line by line.
+        System.IO.StreamReader file =
+           new System.IO.StreamReader(tiedosto);
+        while ((line = file.ReadLine()) != null)
+        {
+          Console.WriteLine(counter  + ":" + line);
+          counter++;
+        }
+
+        file.Close();
+
+        // Suspend the screen.
+        Console.ReadLine();
+      }
+      catch (Exception ex)
+      {
+        throw ex;
+      }
+
+    }
+
+
+    static void LueTiedostoVerStruct(string tiedosto)
     {
       try
       {
         //luetaan tiedosto ja tallennetaan strukteihin
-        // put each line of the text file into a string array. 
+        //each line of the text file into a string array. 
         string[] rivit = System.IO.File.ReadAllLines(tiedosto);
         //käydään läpi
         Ohjastaja kuski;
         int lkm = rivit.Length;
         Console.WriteLine(string.Format("Ohjastajia yhteensä {0}", lkm));
+        //from string array to struct
         for (int i = 3; i < lkm; i++)
         {
           string[] sanat = rivit[i].Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
@@ -52,8 +86,10 @@ namespace JAMK.IT
             kuski.Startit = int.Parse(sanat[1]);
             kuski.Voitot = int.Parse(sanat[2]);
             kuski.VoittoPros = (100F * kuski.Voitot / kuski.Startit);
-            //tulos ulos
-            Console.WriteLine(string.Format("Ohjastaja {0} startit {1} voitot {2} voitto% {3}", kuski.Nimi, kuski.Startit, kuski.Voitot, kuski.VoittoPros));
+            //tulos ulos, pysäytetään 500 ja 1000 kohdalla
+            if (i == 500 || i == 1000)
+            { Console.WriteLine("Press any key to continue"); Console.ReadKey(); }
+            Console.WriteLine(string.Format("{4}. ohjastaja {0} startit {1} voitot {2} voitto% {3}", kuski.Nimi, kuski.Startit, kuski.Voitot, kuski.VoittoPros, i));
           }
         }
         //lopetus
@@ -66,9 +102,8 @@ namespace JAMK.IT
       }
 
     }
-
     //luetaan tiedosto DataTableen DataRow-olioina
-    static void LueTiedostoV1(string tiedosto)
+    static void LueTiedostoVerDT(string tiedosto)
     {
       try
       {
